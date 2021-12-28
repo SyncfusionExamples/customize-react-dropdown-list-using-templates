aed656bb03d464d2cf20b1080f159ce4aadf3a21
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2021-12-28 09:42:18 +0530

    Adding Samples

---
+++ src/data.js
@@ -0,0 +1,15 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.employees=[
+    { "Name": "Andrew Fuller", "EmployeeID": "7", "Designation": "Team Lead", "Country": "England" },
+    { "Name": "Anne Dodsworth", "EmployeeID": "1", "Designation": "Developer", "Country": "USA" },
+    { "Name": "Janet Leverling", "EmployeeID": "3", "Designation": "HR", "Country": "USA" },
+    { "Name": "Laura Callahan", "EmployeeID": "2", "Designation": "Product Manager", "Country": "USA" },
+    { "Name": "Margaret Peacock", "EmployeeID": "6", "Designation": "Developer", "Country": "USA" },
+    { "Name": "Michael Suyama", "EmployeeID": "9", "Designation": "Team Lead", "Country": "USA" },
+    { "Name": "Nancy Davolio", "EmployeeID": "4", "Designation": "Product Manager", "Country": "USA" },
+    { "Name": "Robert King", "EmployeeID": "8", "Designation": "Developer ", "Country": "England" },
+    { "Name": "Steven Buchanan", "EmployeeID": "10", "Designation": "CEO", "Country": "England" }
+]
+
+exports.employees1=[];
